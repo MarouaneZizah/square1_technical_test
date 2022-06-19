4760a4b3c56d2da15926f1adce0cf7cae22640d4
Marouane Zizah <marouane.zizah@tersea.com>
2022-06-20 00:09:21 +0100

    Db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +21,7 @@ class DatabaseSeeder extends Seeder
             'username' => 'admin',
             'email'    => 'admin@test.example',
         ]);
+
+        Post::factory(200)->create();
     }
 }
