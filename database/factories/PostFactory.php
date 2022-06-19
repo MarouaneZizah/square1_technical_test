5f7d892cf685350a6742d55b6913a4721e56263f
Marouane Zizah <marouane.zizah@tersea.com>
2022-06-19 20:41:52 +0100

    add publication_date to post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,24 +19,11 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title'       => $this->faker->sentence(),
-            'slug'        => $this->faker->unique()->slug(),
-            'description' => $this->faker->text(),
-            'user_id'     => User::factory()->create()->id,
+            'title'            => $this->faker->sentence(),
+            'slug'             => $this->faker->unique()->slug(),
+            'description'      => $this->faker->text(),
+            'publication_date' => $this->faker->dateTime(),
+            'user_id'          => User::factory()->create()->id,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
